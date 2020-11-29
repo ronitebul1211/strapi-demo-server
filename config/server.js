aa9367d10547d2d63ba7f156e15e4af0937ef71e
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-29 15:52:28 +0200

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e5ce6f1c6112a6eabb8dd540cd56f00'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '09f741e1-082d-42c8-bb09-63f4e28450ac'
+};
