1a87e591bd7712fef7cd3e1e5995b8455836ae20
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-29 18:19:46 +0200

    override find to Response with the products the user own by sent token

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,21 @@
-'use strict';
-
+"use strict";
+const { default: createStrapi } = require("strapi");
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 /**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
+ * parseMultipartData: This function parses strapi's formData format.
+ * sanitizeEntity: This function removes all private fields from the model and its relations.
  */
 
-module.exports = {};
+/** Response with the products the user own */
+module.exports = {
+  async find(ctx) {
+    let products;
+    if (ctx.query._q) {
+      products = await strapi.services.product.search(ctx.query);
+    } else {
+      products = await strapi.services.product.find(ctx.query);
+    }
+    const userId = ctx.state.user.id;
+    return products.filter((product) => product.user.id === userId);
+  },
+};
