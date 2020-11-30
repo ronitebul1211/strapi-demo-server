4540dd91aa87b910f88158a2848a429cbba29c97
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-30 10:29:12 +0200

    implement search service to search in authorized products based on the user id

--- api/product/services/product.js
+++ api/product/services/product.js
@@ -1,9 +1,15 @@
 "use strict";
 
 module.exports = {
-  //Find all Products for specific user by its id
+  //Find authorized products based on user-id, params
   find(params, userId, populate) {
-    params.user = userId;
-    return strapi.query("product").find(params, populate);
+    const newParams = { ...params, user: userId };
+    return strapi.query("product").find(newParams, populate);
+  },
+
+  //Search in authorized products based on user-id, params
+  search(params, userId) {
+    const newParams = { ...params, user: userId };
+    return strapi.query("product").search(newParams);
   },
 };
