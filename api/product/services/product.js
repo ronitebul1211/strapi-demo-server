bb54d1c6c452bc13b311f3905daa4a2a1048586e
ronitebul1211 <ronitebul1211@gmail.com>
2020-12-01 10:52:51 +0200

    create service to send sms for user with the total count of his products

--- api/product/services/product.js
+++ api/product/services/product.js
@@ -5,4 +5,8 @@ module.exports = {
     const message = `You have successfully added a new product: ${product.title}`;
     return strapi.config.functions.smsApi(message, phone);
   },
+  sendProductsCountSms(count, phone) {
+    const message = `You have a total of ${count} products`;
+    return strapi.config.functions.smsApi(message, phone);
+  },
 };
