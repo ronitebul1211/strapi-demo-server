d4959699ed46e1c02b967285f1d27ec5205e7b88
ronitebul1211 <ronitebul1211@gmail.com>
2020-12-01 10:54:35 +0200

    implement the logic in the send count sms controller to manage sending total products count to specific user

--- api/product/controllers/custom.js
+++ api/product/controllers/custom.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   async sendCountSms(ctx) {
-    return "send product count sms ....";
+    const userId = ctx.state.user.id;
+    const userPhone = ctx.state.user.phoneNumber;
+    const productCount = await strapi.services.product.count({ user: userId });
+    await strapi.services.product.sendProductsCountSms(productCount, userPhone);
+    return "success";
   },
 };
