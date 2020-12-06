8e1e1e3b6e25d70ebcccb4a7dc69eac92df981a8
ronitebul1211 <ronitebul1211@gmail.com>
2020-12-06 17:24:52 +0200

    define the relation in appointment model between instructor, client and user, has one

--- api/appointment/services/appointment.js
+++ api/appointment/services/appointment.js
@@ -8,10 +8,11 @@
 module.exports = {
   sendAppointmentReminderSms(appointment) {
     const appTime = new Date(appointment.date).toLocaleTimeString();
-    const message = `Hi ${appointment.instructor} you have an appointment with ${appointment.client} at ${appTime}`;
-    return strapi.config.functions.smsApi(
-      message,
-      appointment.instructor.phoneNumber
-    );
+    const message = `Hi ${appointment.instructor.username} you have an appointment with ${appointment.client.username} at ${appTime}`;
+    const phoneNumber = appointment.instructor.phoneNumber;
+    console.log(message, phoneNumber);
+    console.log(appointment.instructor);
+    console.log(appointment.client);
+    return strapi.config.functions.smsApi(message, phoneNumber);
   },
 };
