c788c2ec28e91a670672a4543b6f12103c16dea2
ronitebul1211 <ronitebul1211@gmail.com>
2020-12-06 11:06:43 +0200

    format cron file

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,14 +1,4 @@
 "use strict";
-const dateFns = require("date-fns");
-/**
- * Cron config that gives you an opportunity
- * to run scheduled jobs.
- *
- * The cron format consists of:
- * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
- *
- * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#cron-tasks
- */
 
 module.exports = {
   "*/1 * * * * ": async () => {
