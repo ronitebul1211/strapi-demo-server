b7c34def97dbe0af6c0a93ff2e889fa7feb44979
ronitebul1211 <ronitebul1211@gmail.com>
2020-12-06 10:51:05 +0200

    add the logic to send appointnet reminder sms to the instructor

--- api/appointment/services/appointment.js
+++ api/appointment/services/appointment.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  sendAppointmentReminderSms(appointment) {
+    const appTime = new Date(appointment.date).toLocaleTimeString();
+    const message = `Hi ${appointment.instructor} you have an appointment with ${appointment.client} at ${appTime}`;
+    return strapi.config.functions.smsApi(
+      message,
+      appointment.instructor.phoneNumber
+    );
+  },
+};
