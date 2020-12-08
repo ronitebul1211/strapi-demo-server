9f76167211f4cef8d9ae0b17d83d108c8c7ed616
ronitebul1211 <ronitebul1211@gmail.com>
2020-12-08 10:12:46 +0200

    connect app with local postgresSQL DB

--- config/database.js
+++ config/database.js
@@ -1,15 +1,34 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "sqlite",
+//         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        schema: "public",
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
