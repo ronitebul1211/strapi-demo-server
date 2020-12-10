cd8bfdd4892eb4ef00751af55b09adad61772602
ronitebul1211 <ronitebul1211@gmail.com>
2020-12-10 18:42:47 +0200

    specify the absolute url of your backend on server config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("", "http://localhost:1337"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "3e5ce6f1c6112a6eabb8dd540cd56f00"),
